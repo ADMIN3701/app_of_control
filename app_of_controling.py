b7d7f5218542c53aae590c4375e5814cf20ce20a
ADMIN3701 <unknow.ggg.98@gmail.com>
2022-12-13 21:53:22 +0200

    test 4 ?

--- app_of_controling.py
+++ app_of_controling.py
@@ -19,32 +19,60 @@ class MyApp(App):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def build(self):
+        self.Label_text = ""
 
         al = AnchorLayout()
-        bl = BoxLayout(orientation="vertical", padding=25, spacing=25)
-        self.text_input = TextInput(text="Введите текст...")
+        bl_main = BoxLayout(orientation="horizontal", padding=25, spacing=25)
+        bl1 = BoxLayout(orientation="vertical")
+        bl2 = BoxLayout(orientation="vertical")
+
+        self.text_input = TextInput(text="192.168.0.167 1234\nВведите ip адрес и порт")
+        self.text_input2 = TextInput(text="Введите команду")
         self.label = Label()
         self.label2 = Label()
 
+        self.label_split = self.label.text.split(" ")
+        #self.label_split2 = self.label_split.split()
 
-        bl.add_widget(self.label2)
-        self.label2.text = "подключение..."
-        self.client.connect(("192.168.0.167", 1234))
-        self.label2.text = "подключено!"
+        # self.label2.text = "подключение..."
+        # self.client.connect(("192.168.0.167", 1234))
+        # self.label2.text = "подключено!"
 
-        bl.add_widget(self.label)
-        bl.add_widget(self.text_input)
-        bl.add_widget(Button(text="Подтвердить текст", on_press=self.add_operation))
+        bl1.add_widget(self.label)
+        bl1.add_widget(self.text_input)
+        bl1.add_widget(Button(text="Подтвердить текст", on_press=self.add_operation))
 
-        al.add_widget(bl)
+        bl2.add_widget(self.label2)
+        bl2.add_widget(self.text_input2)
+        bl2.add_widget(Button(text="Подтвердить", on_press=self.send_command))
+
+        bl_main.add_widget(bl1)
+        bl_main.add_widget(bl2)
+        al.add_widget(bl_main)
         return al
 
-    def add_operation(self, instate):
-        self.label.text = self.text_input.text
-        print(self.text_input.text)
+    def send_command(self, instate):
+        self.label2.text = self.text_input2.text
+        tttt = self.text_input2.text
+        self.client.send(tttt.encode("utf-8"))
 
-        ttt = self.text_input.text
-        self.client.send(ttt.encode("utf-8"))
+
+    def add_operation(self, instate):
+        self.Label_text += str(self.text_input.text) + "\n"
+        print(self.text_input.text)
+        self.label_update()
+
+        try:
+            self.client.connect(("192.168.0.167", 12341))
+            self.Label_text += "Подключено!"
+        except:
+            self.label.text = "Не удалось подключиться :("
+
+    def label_update(self):
+        self.label.text = self.Label_text
+
+        # ttt = self.text_input.text
+        # self.client.send(ttt.encode("utf-8"))
 
 
 if __name__ == "__main__":
